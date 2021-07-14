f3947d906fc2887b5aabc7820f458e07ed95eb7a
diegonacif <diegoreisnacif@gmail.com>
2021-07-13 22:54:10 -0300

    Criando funcoes e estilizando lista estatica

--- src/App.js
+++ src/App.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdDelete } from "react-icons/md";
 
 import "./App.css";
 
-const App = () => <h1 className="title">App</h1>;
+const App = () => {
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const initialTodos = [
+    { id: 1, title: "Estudar React", checked: false },
+    { id: 2, title: "Estudar Inglês", checked: true },
+    { id: 3, title: "Estudar Guitarra", checked: false },
+    { id: 4, title: "Estudar Python", checked: false },
+  ];
+
+  const [todos] = useState(initialTodos);
+  const [value, setValue] = useState("");
+
+  const erase = () => {
+    setValue("");
+  };
+
+  const submit = () => {
+    console.log(value);
+    erase();
+  };
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.which === ENTER_KEY) {
+      submit();
+    } else if (event.which === ESCAPE_KEY) {
+      erase();
+    }
+  };
+
+  return (
+    <section id="app" className="container">
+      <header>
+        <h1 className="title">Todo</h1>
+      </header>
+      <section className="main">
+        <input
+          className="new-todo"
+          placeholder="O que precisa ser feito?"
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <li key={todo.id.toString()}>
+              <span className="todo">{todo.title}</span>
+                <button className="remove" type="button">
+                  <MdDelete size={23} />
+                </button>
+            </li>              
+          ))}
+        </ul>
+      </section>
+    </section>
+  );
+};
 
 export default App;
