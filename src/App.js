20126b7f2f27a1e204991d0cf873dd30bf5aef19
diegonacif <diegoreisnacif@gmail.com>
2021-07-19 22:24:36 -0300

    finalizado processo de criacao e iniciado processo de refatoracao

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { useState } from "react";
 import { MdDelete } from "react-icons/md";
 
+import NewTodo from "./components/NewTodo";
+
 import "./App.css";
 
 const App = () => {
-  const ESCAPE_KEY = 27;
-  const ENTER_KEY = 13;
-
   const [todos, setTodos] = useState([]);
-  const [value, setValue] = useState("");
 
-  const erase = () => {
-    setValue("");
-  };
-
-  const submit = () => {
+  const onNewTodo = (value) => {
     setTodos([
       ...todos,
       {
@@ -23,20 +17,6 @@ const App = () => {
         checked: false,
       },
     ]);
-
-    erase();
-  };
-
-  const onChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  const onKeyDown = (event) => {
-    if (event.which === ENTER_KEY) {
-      submit();
-    } else if (event.which === ESCAPE_KEY) {
-      erase();
-    }
   };
 
   const onToggle = (todo) => {
@@ -47,19 +27,17 @@ const App = () => {
     );
   };
 
+  const onRemove = (todo) => {
+    setTodos(todos.filter((obj) => obj.id !== todo.id));
+  };
+
   return (
     <section id="app" className="container">
       <header>
         <h1 className="title">Todo</h1>
       </header>
       <section className="main">
-        <input
-          className="new-todo"
-          placeholder="O que precisa ser feito?"
-          value={value}
-          onChange={onChange}
-          onKeyDown={onKeyDown}
-        />
+        <NewTodo onNewTodo={onNewTodo} />
         <ul className="todo-list">
           {todos.map((todo) => (
             <li key={todo.id.toString()}>
@@ -72,7 +50,11 @@ const App = () => {
               >
                 {todo.title}
               </span>
-              <button className="remove" type="button">
+              <button
+                className="remove"
+                type="button"
+                onClick={() => onRemove(todo)}
+              >
                 <MdDelete size={23} />
               </button>
             </li>
