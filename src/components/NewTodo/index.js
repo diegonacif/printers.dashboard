b68d03f9ef2989af43501cd4e06e7ea238cc450f
diegonacif <diegoreisnacif@gmail.com>
2021-07-22 06:23:41 -0300

    finalizando com arquivos de estilo

--- src/components/NewTodo/index.js
+++ src/components/NewTodo/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import "./styles.css";
 
 const NewTodo = ({ onNewTodo }) => {
   const [value, setValue] = useState("");
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { MdDelete } from "react-icons/md";
+import "./styles.css";
 
 const TodoList = ({ todos, onToggle, onRemove }) => (
   <ul className="todo-list">
